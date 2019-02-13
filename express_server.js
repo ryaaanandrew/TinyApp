df8ac1e49584174e39a7d890d67dbb43dae68f4f
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-02-13 21:39:17 +0000

    fixed headers, added log in form checking if user is logged in. added cookie parser

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = 8080;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 
 function generateRandomString() {
     var text = "";
@@ -12,7 +13,7 @@ function generateRandomString() {
     }
     return text;
   }
-
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
@@ -23,22 +24,20 @@ var urlDatabase = {
 };
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies['user_id']};
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  let templateVars = { username: req.cookies['user_id'] }
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL , longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL , longURL: urlDatabase[req.params.shortURL], username: req.cookies['user_id'] };
   res.render('urls_show', templateVars);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
 
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
@@ -61,8 +60,7 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  let username = req.body.username;
-  res.cookie('user_id', username);
+  res.cookie('user_id', req.body.username); // Set-Cookie header
   res.redirect('/urls');
 });
 
